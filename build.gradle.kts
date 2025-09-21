140097451824fd0a2317141d5ee1d0d2cae51a65
hyeonproject <axzswq@gmail.com>
2025-09-21 21:45:22 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("jvm") version "2.2.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["paperweight-mappings-namespace"] = "mojang"
+        attributes["paperweight-mappings-namespace"] = "spigot"
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
---
+++ src/main/kotlin/io/paper/helloworld.kt
@@ -0,0 +1,5 @@
+package io.paper
+
+class helloworld external JavaPlugin {
+
+}
