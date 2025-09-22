61aea20cac18a854d87842ecf4355b1372cf6159
hyeonprojects <axzswq@gmail.com>
2025-09-23 01:12:30 +0900

    fix: ass

--- src/main/kotlin/kr/kro/chzzk/minecraft/message/PlayerMessage.kt
+++ src/main/kotlin/kr/kro/chzzk/minecraft/message/PlayerMessage.kt
@@ -1,4 +1,15 @@
 package kr.kro.chzzk.minecraft.message
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
+import org.bukkit.event.EventHandler
+
+@EventHandler
 class PlayerMessage {
+    fun getVersion(): String {
+        val compoent: Component = Component.text("chzzk 0.1.0 by Hyeonproject")
+            .color(TextColor.color(255, 255, 255))
+            .append(Componet.text(" world!", NamedTextColor.GREEN))
+    }
 }
